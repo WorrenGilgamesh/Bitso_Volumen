b9c270c23bd97f61545ba562809a01b44e890fba
RedTech <redtech@iMac-de-apple-2.local>
2018-05-30 23:12:31 -0500

    todas las monedas

--- margen.py
+++ margen.py
@@ -3,27 +3,33 @@ import datetime
 import decimal
 
 api = bitso.Api()
-trades = api.trades('btc_mxn',limit = '100')
-def transHora(trades):
-    trades2=[]
-    tiempo1=trades[0].created_at
-    for p in trades:
-        tiempo2= p.created_at
-        dif=tiempo1 - tiempo2
-        trades2.append(p)
-        if dif.seconds//3600 ==1:
-            return trades2
+def volBitso(moneda):
+    trades = api.trades(moneda,limit = '100')
+    def transHora(trades):
+        trades2=[]
+        tiempo1=trades[0].created_at
+        for p in trades:
+            tiempo2= p.created_at
+            dif=tiempo1 - tiempo2
+            trades2.append(p)
+            if dif.seconds//3600 ==1:
+                return trades2
 
-def margen(trades):
-    margen =decimal.Decimal(0.0)
-    montoT=decimal.Decimal(0.0)
-    for p in trades:
-        precio= p.price
-        monto = p.amount
-        montoT+=p.amount
-        margen+= precio*monto
-    #return margen*decimal.Decimal(0.1), montoT #exacto
-    return float(margen*decimal.Decimal(0.1)), float(montoT) #aproximado
+    def margen(trades):
+        margen =decimal.Decimal(0.0)
+        montoT=decimal.Decimal(0.0)
+        for p in trades:
+            precio= p.price
+            monto = p.amount
+            montoT+=p.amount
+            margen+= precio*monto
+        #return margen*decimal.Decimal(0.1), montoT #exacto
+        #return float(margen*decimal.Decimal(0.1)), float(montoT) #aproximado
+        return float(margen*decimal.Decimal(0.1))
+    return margen(transHora(trades))
 
-cantidad= margen(transHora(trades))
-print cantidad
+print 'btc_mxn:' , volBitso('btc_mxn')
+print 'eth_mxn:' ,volBitso('eth_mxn')
+print 'xrp_btc:' ,volBitso('xrp_btc')
+print 'ltc_mxn:' ,volBitso('ltc_mxn')
+print 'bch_mxn:' ,volBitso('bch_mxn')
